6a81b2b57af0ce88d2e61dc2c6623df02f422208
Amr El Shimy <amr@meetingbaas.com>
2026-05-08 07:26:44 +0000

    feat: add scenarios to supress exits due to timeout configs according to grace period config

--- src/state-machine/states/waiting-room-state.ts
+++ src/state-machine/states/waiting-room-state.ts
@@ -180,8 +180,11 @@ export class WaitingRoomState extends BaseState {
     // Store the actual start time for later use - It is sent to the backend at the end of the meeting
     GLOBAL.setStartTime(startTime)
 
-    const timeoutMs = GLOBAL.get().waiting_room_timeout * 1000
-    console.info(`Setting waiting room timeout to ${timeoutMs}ms`)
+    const waitingRoomGracePeriodMs = (GLOBAL.get().grace_period ?? 0) * 1000
+    const timeoutMs = GLOBAL.get().waiting_room_timeout * 1000 + waitingRoomGracePeriodMs
+    console.info(
+      `Setting waiting room timeout to ${timeoutMs}ms (includes ${waitingRoomGracePeriodMs}ms grace period)`
+    )
 
     let joinSuccessful = false // Flag indicating we joined the meeting
 
